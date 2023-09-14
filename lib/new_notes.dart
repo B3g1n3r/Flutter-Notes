d77a04160c4a39caf4123fb52b6b33fb9d392fd9
Eswaran <111951792+B3g1n3r@users.noreply.github.com>
2023-09-14 11:45:51 +0530

    Update new_notes.dart

--- lib/new_notes.dart
+++ lib/new_notes.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
-
 class CardItem {
   final String title;
   final String content;
@@ -16,36 +14,5 @@ class CardItem {
   factory CardItem.fromJson(String json) {
     Map<String, dynamic> data = jsonDecode(json);
     return CardItem(title: data['title'], content: data['content']);
-  }
-}
-
-class CardItemWidget extends StatelessWidget {
-  const CardItemWidget({super.key, required this.cardItem});
-
-  final CardItem cardItem;
-
-  @override
-  Widget build(BuildContext context) {
-    return Dismissible(
-      key: Key(cardItem.title),
-      onDismissed: (direction) {
-        
-      },
-      direction: DismissDirection.startToEnd,
-      child: InkWell(
-        child: Card(
-          elevation: 4,
-          margin: const EdgeInsets.all(8),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          child: ListTile(
-            title: Text(cardItem.title,
-                style: const TextStyle(fontWeight: FontWeight.bold)),
-            subtitle: Text(cardItem.content,
-                maxLines: 6, overflow: TextOverflow.ellipsis),
-          ),
-        ),
-      ),
-    );
   }
 }
